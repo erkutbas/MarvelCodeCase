559ca66217b32a17ee1cb46f6a25d91b7add4154
erkutbas <ebas1@binghamton.edu>
2021-09-14 22:32:00 +0300

    Initial Commit

---
+++ MarvelCodeCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelCodeCase
+//
+//  Created by Erkut Bas on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
